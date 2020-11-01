9409e3bf575d2ca754fb16f8835bb99ebff591b5
rom201 <kovalchukr1104@gmail.com>
2020-11-01 18:03:29 -0500

    branch Rom2

--- src/main/java/Rom.java
+++ src/main/java/Rom.java
@@ -1,5 +1,7 @@
 public class Rom {
     public static void main(String[] args) {
         System.out.println("first");
+        System.out.println("first");
+        System.out.println("first");
     }
 }
